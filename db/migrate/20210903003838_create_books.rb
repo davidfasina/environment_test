a0526f358032f2ffcfff5fb4217cf47b9fccf872
root <root@localhost>
2021-09-26 00:04:10 +0000

    Added functionality to add author and book price

--- db/migrate/20210903003838_create_books.rb
+++ db/migrate/20210903003838_create_books.rb
@@ -2,6 +2,8 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
+      t.string :author
+      t.float :price
 
       t.timestamps
     end
