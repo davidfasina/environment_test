0cd5d45453684aaa3ff7a681874436482365fea3
root <root@localhost>
2021-09-30 03:23:38 +0000

    Updated book model and added more test coverage

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,8 +5,14 @@ RSpec.describe 'Creating a book', type: :feature do
     scenario 'valid inputs' do
       visit new_book_path
       fill_in 'Title', with: 'harry potter'
+      fill_in 'Author', with: 'RHQ'
+      fill_in 'Price', with: '14.29'
+      fill_in 'Published', with: '2000-11-11'
       click_on 'Create Book'
       visit books_path
       expect(page).to have_content('harry potter')
+      expect(page).to have_content('RHQ')
+      expect(page).to have_content('14.29')
+      expect(page).to have_content('2000-11-11')
     end
 end
